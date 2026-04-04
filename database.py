e6df12e6f89f2bf85373c3a519ce462a85d92e14
2Kelvin <rocketmancodes02@gmail.com>
2026-04-04 10:15:20 +0300

    fix database deprecation warning

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 # Replace with your actual Postgres details
 SQLALCHEMY_DATABASE_URL = "postgresql://kev:1234@postgres/all_items"
